1ef28cbeeb9dc40c7aefd63de36b73794a0e891e
charlupuhh <62491397+charlupuhh@users.noreply.github.com>
2020-06-24 10:39:53 -0700

    finished list ish

--- src/Components/List.js
+++ src/Components/List.js
@@ -2,12 +2,49 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import * as Yup from 'yup'
 import Item from './Item'
+import { v4 as uuid } from 'uuid'
 
+// {
+//     name: 'name',
+//     ID: 'integer',
+//     completed: 'true/false',
+//     created_at: 'time',
+//     updated_at: 'time',
+//     userID: 'integer',
+//     recurring: 'dropdown of [single time, daily, monthly, weekly]'
+// }
+const initialItems = [
+    {
+        name: 'Buy food',
+        body: 'remember to buy food',
+        completed: false,
+        recurring: 'weekly',
+        due_date: 'before you run out of food'
+    },
+    {
+        name: 'Cook food',
+        body: 'remember to cook food',
+        completed: false,
+        recurring: 'daily',
+        due_date: 'before you starve'
+    }
+]
 export default function List(){
+
+    const [itemList, updateItemList] = useState([])
+
+    //Initialize first item list
+    useEffect(() => {
+        updateItemList(initialItems)
+    },[])
+    
     return(
         <div>
-            <Item num='1'/>
-            <Item num='2'/>
+            {itemList.map(item => {
+                return (
+                    <Item itemInfo={item} />
+                )
+            })}
         </div>
     )
 }
