3da0b1e1275a3020c2a7d1b2d11782969c39d1a1
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-06-25 13:48:15 -0400

    fixed the validation

--- src/Components/Validation/loginSchema.js
+++ src/Components/Validation/loginSchema.js
@@ -4,9 +4,9 @@ const loginSchema = Yup.object().shape({
   username: Yup.string()
     .min(1, "Username is required")
     .required("Username is required"),
-  email: Yup.string(),
-  // .email("Must be a valid email address.")
-  // .required("Must include email address.")
+  email: Yup.string()
+    .email("Must be a valid email address.")
+    .required("Must include email address."),
   password: Yup.string()
     .min(6, "Password must be at least 6 characters long")
     .required("Password is required"),
